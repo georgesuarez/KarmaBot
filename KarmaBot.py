98c3aed3ea1176a9dee3c41276eaf3863eb2d011
georgesuarez <georgejsuarez@gmail.com>
2017-11-03 23:22:29 -0700

    Formatted code to PEP8 style

--- KarmaBot.py
+++ KarmaBot.py
@@ -16,6 +16,7 @@ bot_prefix = '!'
 
 bot = commands.Bot(description=description, command_prefix=bot_prefix)
 
+
 @bot.event
 async def change_presence():
     await bot.change_presence(game=discord.Game(name='Beep-bop'))
@@ -32,6 +33,8 @@ redditbot = praw.Reddit(client_id=prawbot.client_id,
                         user_agent=prawbot.user_agent)
 
 """Log in the discord bot"""
+
+
 @bot.event
 async def on_ready():
     print('Logged in')
@@ -43,19 +46,24 @@ async def on_ready():
 """Bot commands go here"""
 # Clear messages up to 14 days old
 # Can only clear up to 2 to 100 messages at a time
-@bot.command(pass_context = True)
-async def clear(ctx, number):
-   number = int(number)
-   messages_to_delete = []
 
-   async for x in bot.logs_from(ctx.message.channel, limit=number):
-       messages_to_delete.append(x)
-   await bot.delete_messages(messages_to_delete)
+
+@bot.command(pass_context=True)
+async def clear(ctx, number):
+    number = int(number)
+    messages_to_delete = []
+
+    async for x in bot.logs_from(ctx.message.channel, limit=number):
+        messages_to_delete.append(x)
+    await bot.delete_messages(messages_to_delete)
 
 # Imugr command v1.0
+
+
 @bot.command()
-async def imgur(query : str):
-    subreddit = client.subreddit_gallery(query, sort='top', window='day', page=0)
+async def imgur(query: str):
+    subreddit = client.subreddit_gallery(
+        query, sort='top', window='day', page=0)
 
     url_links = []
     title_of_links = []
@@ -72,8 +80,10 @@ async def imgur(query : str):
     await bot.say(url_link)
 
 # Reddit command v1.0
+
+
 @bot.command()
-async def reddit(query : str):
+async def reddit(query: str):
     subreddit = redditbot.subreddit(query)
     url_links = []
     title_of_links = []
@@ -88,6 +98,6 @@ async def reddit(query : str):
 
     await bot.say(title)
     await bot.say(url_link)
-        
+
 # Always put this last!!!
 bot.run(bot_token.token)
