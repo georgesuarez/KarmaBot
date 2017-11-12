c06d94f2601c921150174b10d672931ad82b38b4
georgesuarez <georgejsuarez@gmail.com>
2017-11-12 15:25:08 -0800

    Need a way to change the playing status of the bot

--- KarmaBot.py
+++ KarmaBot.py
@@ -3,24 +3,26 @@ import random
 import requests
 import praw
 import asyncio
+
 import imgurbot
 import prawbot
 import bot_token
 
-from bs4 import BeautifulSoup
-from discord.ext import commands
+from discord.ext.commands import Bot
 from imgurpython import ImgurClient
+from imgurpython.helpers.error import ImgurClientError
 
 description = 'KarmaBot'
 bot_prefix = '!'
 
-bot = commands.Bot(description=description, command_prefix=bot_prefix)
+bot = Bot(description=description, command_prefix=bot_prefix)
 
+client = discord.Client()
 
 """Authorizing Imgur API"""
 client_id = imgurbot.client_id
 client_secret = imgurbot.client_secret
-client = ImgurClient(client_id, client_secret)
+imgur_client = ImgurClient(client_id, client_secret)
 
 """Authorizing Reddit API"""
 redditbot = praw.Reddit(client_id=prawbot.client_id,
@@ -28,6 +30,8 @@ redditbot = praw.Reddit(client_id=prawbot.client_id,
                         user_agent=prawbot.user_agent)
 
 """Log in the discord bot"""
+
+
 @bot.event
 async def on_ready():
     print('Logged in')
@@ -35,12 +39,12 @@ async def on_ready():
     print('ID : {}'.format(bot.user.id))
     print(discord.__version__)
     print('========')
-    await bot.change_presence(game=discord.Game(name='!help'))
-
 
 """Bot commands go here"""
+
 # Clear messages up to 14 days old
 # Can only clear up to 2 to 100 messages at a time
+
 @bot.command(pass_context=True)
 async def clear(ctx, number):
     number = int(number)
@@ -50,44 +54,46 @@ async def clear(ctx, number):
         messages_to_delete.append(x)
     await bot.delete_messages(messages_to_delete)
 
-# Imugr command v1.0
+
+# TODO: Find a to change the playing status of the bot
+@bot.command()
+async def game(*, game : str):
+    await bot.change_presence(game=discord.Game(name=game))
+    await bot.say('Changing game to: ' + game)
+
+
+# Imugr command v1.0.1
+
+
 @bot.command()
 async def imgur(query: str):
-    
-    subreddit = client.subreddit_gallery(query, sort='top', window='day', page=0)
+
+    subreddit = imgur_client.subreddit_gallery(
+        query, sort='top', window='day', page=0)
 
     url_links = []
-    title_of_links = []
 
     for submission in subreddit:
-        title_of_links.append(submission.title)
         url_links.append(submission.link)
 
-    submissions = dict(zip(title_of_links, url_links))
+    url_link = random.choice(url_links)
 
-    title, url_link = random.choice(list(submissions.items()))
-
-    await bot.say(title)
     await bot.say(url_link)
 
-# Reddit command v1.0
+# Reddit command v1.0.1
+
+
 @bot.command()
 async def reddit(query: str):
     subreddit = redditbot.subreddit(query)
     url_links = []
-    title_of_links = []
 
     for submission in subreddit.top(limit=50):
-        title_of_links.append(submission.title)
         url_links.append(submission.url)
 
-    submissions = dict(zip(title_of_links, url_links))
+    url_link = random.choice(url_links)
 
-    title, url_link = random.choice(list(submissions.items()))
-
-    await bot.say(title)
     await bot.say(url_link)
-
 
 # Always put this last!!!
 bot.run(bot_token.token)
